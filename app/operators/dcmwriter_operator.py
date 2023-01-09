fa481ad5b6a5f3c98d907ef493036d32b2bbb888
Tom Roberts <t.roberts@kcl.ac.uk>
2023-01-09 13:42:36 +0000

    WIP: skeleton of final operator

--- app/operators/dcmwriter_operator.py
+++ app/operators/dcmwriter_operator.py
@@ -18,10 +18,19 @@ class DicomWriterOperator(Operator):
 
         logging.info(f"Begin {self.compute.__name__}")
 
+        input_path = op_input.get("input_files").path
+
+        nii_output_path = input_path / 'nii_output'
+
+        dcm_output_path = op_output.get().path
+
+        logging.info(f"WIP: Here is where nii2dcm will be performed on TotalSegmentator output.")
+
         pass
 
         # TODO:
         #  - implement nii2dcm to convert .nii.gz labels into DICOMs files that match original
         #  - OR: use the existing label tools within MONAI Deploy
+        #  - something like nii2dcm(nii_output_path, dcm_output_path, 'CT')
 
         logging.info(f"End {self.compute.__name__}")
