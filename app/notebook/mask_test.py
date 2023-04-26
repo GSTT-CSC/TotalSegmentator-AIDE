2f4fa7328425eba3ebaa25e34fda94a46a047081
AnilMistry <79843755+AnilMistry@users.noreply.github.com>
2023-04-26 09:08:34 +0100

    Update mask_test.py

--- app/notebook/mask_test.py
+++ app/notebook/mask_test.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import nibabel as nib
 
+""" Test script to generate view"""
+
+
 def add_masks():
+    # read in ct nifti and create masks list
     ct_nifti_filename = "../../input/dcm_input/input-ct-dataset.nii.gz"
     mask_path_dir = "../../input/nii_seg_output/"
     dcm_meta = pydicom.dcmread("../../input/dcm_input/CT.1.2.246.352.71.3.815247573688.27173.20160407133501.dcm",
@@ -17,6 +21,7 @@ def add_masks():
     img = nib.load(ct_nifti_filename)
     a = np.array(img.dataobj)
 
+    # Calculate aspect ratios
     ps = dcm_meta.PixelSpacing
     ss = dcm_meta.SliceThickness
 
@@ -31,13 +36,13 @@ def add_masks():
     sag_arr = np.rot90(a[:, int(a.shape[1] / 2), :])
     cor_arr = np.rot90(a[:, :, int(a.shape[2] / 2)])
 
-    # display on separate images
-    a1 = plt.subplot(3,1, 1)
+    # display projections
+    a1 = plt.subplot(3, 1, 1)
     plt.axis('off')
     plt.imshow(cor_arr, cmap='gray')
     a1.set_aspect(ax_aspect)
 
-    a2 = plt.subplot(3,1, 2)
+    a2 = plt.subplot(3, 1, 2)
     plt.axis('off')
     plt.imshow(sag_arr, cmap='gray')
     a2.set_aspect(sag_aspect)
@@ -48,32 +53,36 @@ def add_masks():
     a3.set_aspect(cor_aspect)
 
     # display all contours  ------------------
-    color_list = plt.colormaps()
-    max_val = len(masks)
-    for i,mask in enumerate(masks):
+    # repeats above for all masks
+
+    max_val = len(masks)    # sets upper limit of colour scale on cmap = 'hsv'
+    alpha = 0.3             # transparency of contour
+
+    for i, mask in enumerate(masks):
         try:
             print(mask)
             img = nib.load(mask)
             b = np.array(img.dataobj)
-            b= b*i # means each mask is a different value, therefore different colour on cmap = 'hsv'
+            b = b * i        # means each mask is a different value, therefore different colour on cmap = 'hsv'
 
-            pax_arr = np.rot90(b[int(b.shape[0] / 2), :, :])
-            pax_arr_masked = np.ma.masked_where(pax_arr == 0, pax_arr)
-            psag_arr = np.rot90(b[:, int(b.shape[1] / 2), :])
-            psag_arr_masked = np.ma.masked_where(psag_arr == 0, psag_arr)
-            pcor_arr = np.rot90(b[:, :, int(b.shape[2] / 2)])
-            pcor_arr_masked = np.ma.masked_where(pcor_arr == 0, pcor_arr)
+            c_ax_arr = np.rot90(b[int(b.shape[0] / 2), :, :])
+            c_ax_arr_masked = np.ma.masked_where(c_ax_arr == 0, c_ax_arr)
+            c_sag_arr = np.rot90(b[:, int(b.shape[1] / 2), :])
+            c_sag_arr_masked = np.ma.masked_where(c_sag_arr == 0, c_sag_arr)
+            c_cor_arr = np.rot90(b[:, :, int(b.shape[2] / 2)])
+            c_cor_arr_masked = np.ma.masked_where(c_cor_arr == 0, c_cor_arr)
 
             # display on separate images
             a1 = plt.subplot(3, 1, 1)
-            plt.imshow(pcor_arr_masked, cmap='hsv', alpha=0.3, interpolation='none', vmin=0, vmax=max_val)
+            plt.imshow(c_cor_arr_masked, cmap='hsv', alpha=alpha, interpolation='none', vmin=0, vmax=max_val)
             a1.set_aspect(ax_aspect)
-            a2 = plt.subplot(3,1, 2)
-            plt.imshow(psag_arr_masked, cmap='hsv', alpha=0.3, interpolation='none', vmin=0, vmax=max_val)
+            a2 = plt.subplot(3, 1, 2)
+            plt.imshow(c_sag_arr_masked, cmap='hsv', alpha=alpha, interpolation='none', vmin=0, vmax=max_val)
             a2.set_aspect(sag_aspect)
-            a3 = plt.subplot(3,1, 3)
-            plt.imshow(pax_arr_masked, cmap='hsv', alpha=0.3, interpolation='none', vmin=0, vmax=max_val)
+            a3 = plt.subplot(3, 1, 3)
+            plt.imshow(c_ax_arr_masked, cmap='hsv', alpha=alpha, interpolation='none', vmin=0, vmax=max_val)
             a3.set_aspect(cor_aspect)
+
         except IndexError:
             print(f"failed on {i} {mask}")
             continue
