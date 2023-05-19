a7343423f447b6b9ab9675cc2ace1e29e27a534b
AnilMistry <79843755+AnilMistry@users.noreply.github.com>
2023-05-19 16:20:01 +0100

    remove local paths

--- app/operators/totalsegmentator_operator.py
+++ app/operators/totalsegmentator_operator.py
@@ -29,21 +29,20 @@ class TotalSegmentatorOperator(Operator):
             logging.info(f"Found nii_input_file: {nii_input_file}")
 
         # Create TotalSegmentator output directory
-        output_folder = op_output.get_group_path()
-        nii_seg_output_path = os.path.join(output_folder, "nii_seg_output")
+        nii_seg_output_path = "nii_seg_output"
         if not os.path.exists(nii_seg_output_path):
             os.makedirs(nii_seg_output_path)
 
         # Run TotalSegmentator
-        #subprocess.run(["TotalSegmentator", "-i", nii_input_file, "-o", nii_seg_output_path])
+        subprocess.run(["TotalSegmentator", "-i", nii_input_file, "-o", nii_seg_output_path])
 
         # Local testing - copy from local_files directory e.g. local_files in repo root (hardcode this yourself)
-        src = '/Users/anil/Documents/GitHub/TotalSegmentator-AIDE/local_files/nii_seg_output'
-        for file in os.listdir(src):
-            shutil.copy2(os.path.join(src, file), nii_seg_output_path)
+        # src = '../../local_files/nii_seg_output'
+        # for file in os.listdir(src):
+        #    shutil.copy2(os.path.join(src, file), nii_seg_output_path)
 
         logging.info("Performed TotalSegmentator processing")
 
         # Set output path for next operator
-        op_output.set(DataPath(nii_seg_output_path), "nii_seg_output_path")
+        op_output.set(DataPath(os.path.abspath(nii_seg_output_path)), "nii_seg_output_path")
         logging.info(f"End {self.compute.__name__}")
