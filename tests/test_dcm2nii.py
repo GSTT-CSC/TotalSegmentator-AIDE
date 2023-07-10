3f96c2e936fc56f746e2e6ac6e67f568e4b9e834
Tom Roberts <t.roberts@kcl.ac.uk>
2023-07-10 13:17:16 +0100

    Switch to pytest / adds test_load_nifti()

--- tests/test_dcm2nii.py
+++ tests/test_dcm2nii.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import unittest
 import subprocess
 import nibabel as nib
 import numpy as np
@@ -8,15 +7,15 @@ import numpy as np
 from pathlib import Path
 
 
-class TestDcm2NiiConversion(unittest.TestCase):
+class TestDcm2NiiConversion:
+    def setup_method(self):
+        self.dcm_ref_path = Path('data/dcm2nii/dcm/')
+        self.nii_ref_file = Path('data/dcm2nii/nii/ct-test-data.nii.gz')
+        self.nii_gen_path = Path('data/dcm2nii/nii_generated/')
 
-    def setUp(self):
-        self.dcm_ref_path = Path('tests/data/dcm2nii/dcm/')
-        self.nii_ref_file = Path('tests/data/dcm2nii/nii/ct-test-data.nii.gz')
-        self.nii_gen_path = Path('tests/data/dcm2nii/nii_generated/')
-
-    def tearDown(self):
-        shutil.rmtree(self.nii_gen_path)
+    def test_load_nifti(self):
+        nii_obj = nib.load(self.nii_ref_file)
+        assert issubclass(type(nii_obj), nib.nifti1.Nifti1Image)
 
     def test_dcm2niix(self):
         os.makedirs(self.nii_gen_path, exist_ok=True)
@@ -25,8 +24,7 @@ class TestDcm2NiiConversion(unittest.TestCase):
         img_ref = nib.load(self.nii_ref_file).get_fdata()
         img_gen = nib.load(self.nii_gen_path / 'generated-ct-test-data.nii.gz').get_fdata()
         images_equal = np.array_equal(img_ref, img_gen)
-        self.assertTrue(images_equal, "dcm2niix conversion issue – reference and generated nii files not equal")
+        assert images_equal, f"Generated image array not equal to reference image array"
 
-
-if __name__ == '__main__':
-    unittest.main()
+        # remove test generated images
+        shutil.rmtree(self.nii_gen_path)
